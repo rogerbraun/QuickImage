6fd89545be9e24e2637a9ba2d699e625f0f21c16
Roger Braun <davinelulinvega@gmail.com>
2012-02-07 21:50:31 +0100

    nicer look, more features

--- app.rb
+++ app.rb
@@ -6,24 +6,30 @@ require "cgi"
 Bundler.require
 
 get "/" do
-  "quickimage.heroku.com/keyword"
+  redirect "/index.html"
 end
 
 get "/favicon.ico" do
   halt 404
 end
 
-get "/:keyword" do
-  random_result params["keyword"]
+get "/image/:key/:width/:height" do
+  result = random_result params[:key]
+  result_base = File.basename(result)
+  x_y = params[:width] + "x" + params[:height]
+  filename = params[:key] + "_" + x_y + ".jpg"
+  `wget -P tmp #{result}` 
+  `convert -resize #{x_y} -extent #{x_y} -gravity center tmp/#{result_base} tmp/#{filename}`
+  send_file "tmp/" + filename
 end
 
 def search keyword 
   url = "https://www.google.com/search?q=#{CGI::escape keyword}&tbm=isch"
   results = open(url).read.scan /imgurl=(.+?)&/
+  results.flatten
 end
 
 def random_result keyword 
   res = search keyword 
-  pick = res[rand(res.size)].first
-  "<img src='#{pick}' />"
+  pick = res[rand(res.size)]
 end
