55fd5d6f04c462626692dc3f31d0088b3dcd91f0
Roger Braun <roger@rogerbraun.net>
2012-02-08 14:07:28 +0100

    HIERRRR

--- app.rb
+++ app.rb
@@ -13,16 +13,37 @@ get "/favicon.ico" do
   halt 404
 end
 
+get "/image/:key/:width/:height/hier" do
+  file = get_random_file params[:key]
+  file = add_hier file
+  send_file file
+end
+
 get "/image/:key/:width/:height" do
-  result = random_result params[:key]
-  result_base = File.basename(result)
-  x_y = params[:width] + "x" + params[:height]
-  filename = result_base + "_" + x_y + ".jpg"
-  unless File.exists?(File.join("tmp", "result_base"))
-    `wget -P tmp #{result}`
-    `convert -resize #{x_y} -extent #{x_y} -gravity center tmp/#{result_base} tmp/#{filename}`
+  file = get_random_file params[:key]
+  send_file file
+end
+
+def add_hier file
+  `composite label:HIER!!!! -gravity center #{file} #{file}_hier.jpg`
+  file + "_hier.jpg"
+end
+
+def get_random_file keyword
+  converted = false
+  while not converted do
+    result = random_result keyword
+    result_base = File.basename(result)
+    x_y = params[:width] + "x" + params[:height]
+    filename = result_base + "_" + x_y + ".jpg"
+    unless File.exists?(File.join("tmp", result_base))
+      `wget -P tmp #{result}`
+      `convert -resize #{x_y} -extent #{x_y} -gravity center tmp/#{result_base} tmp/#{filename}`
+    end
+    converted = true if File.exists?(File.join("tmp",filename))
   end
-  send_file "tmp/" + filename
+
+  File.join "tmp", filename
 end
 
 def search keyword 
